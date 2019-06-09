988f7fcc832f1a7f666261a712dbb025f355601e
yangtianrui <yangtianrui95@gmail.com>
2019-06-10 00:00:42 +0800

    MusicBar popup window

--- src/components/MusicBar.vue
+++ src/components/MusicBar.vue
@@ -7,20 +7,41 @@
     </div>
 
     <div class="play-panel">
-      <span>列表</span>
+      <span @click="togglePlayList">列表</span>
       <span class="play" @click="onPlayClick">{{play ? '暂停' : '播放' }}</span>
       <span>切换</span>
     </div>
+
+    <!--弹出动画-->
+    <transition name="slideTop">
+      <div class="play-list" v-if="showPlayList">
+        <div class="popup-header">
+          <span class="clear">清空</span>
+          <span class="list-tip">播放列表<span class="list-num">({{totalCount}}首)</span></span>
+          <!--TODO 替换icon-->
+          <span class="more">更多</span>
+        </div>
+        <ul class="popup-list">
+          <li v-for="item in playList">
+            <span>{{item.sound.name}}</span>
+            <!--TODO 替换icon-->
+            <span class="delete">x</span>
+          </li>
+        </ul>
+      </div>
+    </transition>
+
     <audio autoplay :src="audio_data.sound.source" id="audio"></audio>
   </div>
 </template>
 
 <style scoped lang="stylus">
+
+  $musicbarHeight = 100px
   .music-bar {
     background: white
-    opacity .9
     width: 768px;
-    height: 100px
+    height: $musicbarHeight
     display flex
     flex-direction row
     align-items center
@@ -63,12 +84,71 @@
         display flex
         border: 1px solid #aaa
         border-radius 100%
+        cursor pointer
       }
       .play {
         width: 75px
         height: 75px
       }
     }
+
+    .play-list {
+      font-size 0.33rem
+      position absolute
+      width: 100%
+      bottom: $musicbarHeight
+      opacity 1
+      background: white
+
+      .popup-header {
+        $marginToBoound = 15px
+        text-align center
+        margin-top $marginToBoound
+        span {
+          color #6ed56c
+          .list-num {
+            font-size 0.3rem
+          }
+          &.clear {
+            float: left
+            margin-left $marginToBoound
+          }
+          &.more {
+            float: right
+            margin-right $marginToBoound
+          }
+        }
+      }
+
+      .popup-list {
+        max-height: 350px
+        overflow scroll
+        // 移除横向滚动条
+        overflow-x hidden
+        margin 0
+        padding 0
+
+        li {
+          padding: 15px
+          border-bottom: 1px solid #f4f4f4;
+          .delete {
+            float: right
+          }
+        }
+      }
+    }
+  }
+
+  .slideTop-enter-active, .slideTop-leave-active {
+    transition: all .5s;
+    bottom: 0
+  }
+
+  .slideTop-enter, .slideTop-leave-to {
+    opacity 0
+  // 存在选择器被覆盖不生效问题
+    bottom: 0 !important
+    position absolute !important
   }
 </style>
 
@@ -87,13 +167,19 @@
     },
 
     data() {
-      return {}
+      return {
+        showPlayList: true
+      }
     },
 
     methods: {
       ...mapMutations([
         mutation.SET_AUDIO_ELE
       ]),
+
+      togglePlayList() {
+        this.showPlayList = !this.showPlayList;
+      },
 
       onPlayClick() {
         console.log('onPlayClick', this.play);
@@ -106,7 +192,22 @@
         this.$nextTick(() => {
           console.log('audioInit ---> $nextTick', this.$el);
           const audio = this.$el.querySelector('#audio');
-          window.aa = audio;
+          audio.onplay = () => {
+            console.log('audio element onplay');
+          };
+          audio.onpause = () => {
+            if (this.play) {
+              this.$emit('playStatusChange', false);
+            }
+            console.log('audio element onpause');
+          };
+          audio.onended = () => {
+            console.log('audio element onended');
+            // TODO 继续播放下一首
+          };
+          audio.ontimeupdate = () => {
+            console.log('audio element ontimeupdate')
+          };
           console.log(audio);
           this[mutation.SET_AUDIO_ELE](audio);
         })
@@ -119,13 +220,31 @@
           console.log('MusicBar.vue mapState audio_data');
           return state.audio.data;
         },
-      })
+        audio_ele: state => state.audio.ele,
+      }),
+
+      ...mapState(['playList']),
+
+      totalCount() {
+        return 0;
+      }
     },
     watch: {
       audio_data(val) {
         console.log('watch audio_data');
         if (val) {
           this.audioInit();
+        }
+      },
+      play(val) {
+        console.log('watch play ', val);
+        if (!this.audio_ele) {
+          return;
+        }
+        if (!val) {
+          this.audio_ele.pause();
+        } else {
+          this.audio_ele.play();
         }
       }
     }
